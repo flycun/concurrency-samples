353e6a77ee75f1e938caa2d5825b18302f9833aa
flycun <flycun@163.com>
2019-06-19 14:49:02 +0800

    add volatile example

--- src/main/java/com/music/VolatileFeaturesExample1.java
+++ src/main/java/com/music/VolatileFeaturesExample1.java
@@ -1,17 +1,17 @@
 package com.music;
 
 class VolatileFeaturesExample1 {
-    volatile long vl = 0L; //使用volatile声明64位的long型变量
+    volatile long vl = 0L;
 
     public void set(long l) {
-        vl = l;   //单个volatile变量的写
+        vl = l;
     }
 
     public void getAndIncrement() {
-        vl++;    //复合（多个）volatile变量的读/写
+        vl++;    //复合volatile变量的读/写
     }
 
     public long get() {
-        return vl;   //单个volatile变量的读
+        return vl;
     }
 }
