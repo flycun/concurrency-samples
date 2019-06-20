2b862c79624bdec2c20d8ea4cacf56f7da0c543e
flycun <flycun@163.com>
2019-06-20 19:04:32 +0800

    add Account lock by two

--- src/main/java/com/music/Account.java
+++ src/main/java/com/music/Account.java
@@ -4,10 +4,12 @@ class Account {
     private int balance;
 
     void transfer(Account target, int money) {
-        synchronized (Account.class) {
-            if (this.balance > money) {
-                this.balance -= money;
-                target.balance += money;
+        synchronized (this) { //锁定转出账户
+            synchronized (target){//锁定转入账户
+                if (this.balance > money) {
+                    this.balance -= money;
+                    target.balance += money;
+                }
             }
         }
     }
