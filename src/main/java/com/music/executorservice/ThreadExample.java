453f834fe523699915ac7767e98ed2c602302f21
flycun <flycun@163.com>
2019-06-23 12:05:33 +0800

    add ThreadExample

---
+++ src/main/java/com/music/executorservice/ThreadExample.java
@@ -0,0 +1,16 @@
+package com.music.executorservice;
+
+public class ThreadExample {
+    public static void main(String[] args) {
+        Thread thread1 = new Thread(new Task());
+        thread1.start();
+        System.out.println("Thread Name:" + Thread.currentThread().getName());
+    }
+
+    static class Task implements Runnable {
+        @Override
+        public void run() {
+            System.out.println("Thread Name:" + Thread.currentThread().getName());
+        }
+    }
+}
