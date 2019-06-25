374152058d02ce84ed40e8fead5351c2ba618e8c
flycun <flycun@163.com>
2019-06-25 17:52:26 +0800

    update

--- src/main/java/com/music/locks/SafeSequenceWithoutLock.java
+++ src/main/java/com/music/locks/SafeSequenceWithoutLock.java
@@ -5,11 +5,9 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class SafeSequenceWithoutLock {
 
     private final AtomicInteger counter = new AtomicInteger(0);
-    
-    public int getCount() {
+        public int getCount() {
         return counter.get();
     }
-    
     public void increamentCount() {
        counter.incrementAndGet();
     }
